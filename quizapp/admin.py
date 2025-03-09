5364302d4dac2f54726c240fea4ac74a89a4b894
lanang <muh.lanangzalkifla@gmail.com>
2025-03-09 12:35:26 +0700

    fix error (2)

--- quizapp/admin.py
+++ quizapp/admin.py
@@ -1,13 +1,6 @@
 from django.contrib import admin
-<<<<<<< HEAD
 from quizapp.models import *
 
 # Register your models here.
 admin.site.register(Tryout)
 admin.site.register(Question)
-=======
-from quizapp.models import Tryout
-
-# Register your models here.
-admin.site.register(Tryout)
->>>>>>> d4edcb778130b88afc19c4edaaeb436b985c8af6
